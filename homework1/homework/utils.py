fa4971ffe93509853f8cce6e6833df58e83fa3ff
MallikaUT <mallika.chaulagain@utexas.edu>
2023-09-01 04:27:05 +0000

    utils change

--- homework1/homework/utils.py
+++ homework1/homework/utils.py
@@ -4,12 +4,13 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import csv
 import torch
+import os
 
 LABEL_NAMES = ['background', 'kart', 'pickup', 'nitro', 'bomb', 'projectile']
 
 
 class SuperTuxDataset(Dataset):
-    def __init__(self, dataset_path):
+    def __init__(self, dataset_path,transform=None):
         """
         Your code here
         Hint: Use the python csv library to parse labels.csv
@@ -19,11 +20,15 @@ class SuperTuxDataset(Dataset):
         self.dataset_path = dataset_path
         self.data = []
         
-        with open(dataset_path + '/labels.csv', 'r') as file:
+        with open(os.path.join(dataset_path, 'labels.csv'), 'r') as file:
             csv_reader = csv.reader(file)
             next(csv_reader)  # Skip header row
             for row in csv_reader:
-                self.data.append((row[0], int(row[1])))
+                image_path = os.path.join(dataset_path, row[0])
+                label = int(row[1])
+                self.data.append((image_path, label))
+
+        self.transform = transform
 
 
         raise NotImplementedError('SuperTuxDataset.__init__')
@@ -41,9 +46,9 @@ class SuperTuxDataset(Dataset):
         return a tuple: img, label
         """
         image_path, label = self.data[idx]
-        image = torch.load(self.dataset_path + '/' + image_path)
-        image = image.permute(2, 0, 1)  # Reshape to (3, 64, 64)
-        image = image.float() / 255.0  # Normalize to [0, 1]
+        image = Image.open(image_path).convert('RGB')  # Open the image using PIL
+        if self.transform:
+            image = self.transform(image)
         return image, label
         raise NotImplementedError('SuperTuxDataset.__getitem__')
 

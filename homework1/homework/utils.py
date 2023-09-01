0570421a19ec5b34848b42b594fff837593b7890
MallikaUT <mallika.chaulagain@utexas.edu>
2023-09-01 03:44:54 +0000

    utils.py

--- homework1/homework/utils.py
+++ homework1/homework/utils.py
@@ -2,6 +2,8 @@ from PIL import Image
 
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+import csv
+import torch
 
 LABEL_NAMES = ['background', 'kart', 'pickup', 'nitro', 'bomb', 'projectile']
 
@@ -14,12 +16,23 @@ class SuperTuxDataset(Dataset):
 
         WARNING: Do not perform data normalization here. 
         """
+        self.dataset_path = dataset_path
+        self.data = []
+        
+        with open(dataset_path + '/labels.csv', 'r') as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)  # Skip header row
+            for row in csv_reader:
+                self.data.append((row[0], int(row[1])))
+
+
         raise NotImplementedError('SuperTuxDataset.__init__')
 
     def __len__(self):
         """
         Your code here
         """
+        return len(self.data)
         raise NotImplementedError('SuperTuxDataset.__len__')
 
     def __getitem__(self, idx):
@@ -27,6 +40,11 @@ class SuperTuxDataset(Dataset):
         Your code here
         return a tuple: img, label
         """
+        image_path, label = self.data[idx]
+        image = torch.load(self.dataset_path + '/' + image_path)
+        image = image.permute(2, 0, 1)  # Reshape to (3, 64, 64)
+        image = image.float() / 255.0  # Normalize to [0, 1]
+        return image, label
         raise NotImplementedError('SuperTuxDataset.__getitem__')
 
 

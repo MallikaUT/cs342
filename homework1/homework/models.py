02ec4ea714e79e74b19878150b94cd9a9bdd00fc
MallikaUT <mallika.chaulagain@utexas.edu>
2023-09-02 05:20:37 +0000

    message

--- homework1/homework/models.py
+++ homework1/homework/models.py
@@ -54,10 +54,19 @@ class MLPClassifier(torch.nn.Module):
         """
         Your code here
         """
-        super().__init__()
+        super(MLPClassifier, self).__init__()
+        #super().__init__()
         self.fc1 = torch.nn.Linear(3 * 64 * 64, 128)
         self.fc2 = torch.nn.Linear(128, 64)
         self.fc3 = torch.nn.Linear(64, 6)  # Output size: 6
+        
+        # Define the layers of the MLP
+        self.fc1 = nn.Linear(3 * 64 * 64, 128)  # First hidden layer (input size to 128)
+        self.relu1 = nn.ReLU()  # ReLU activation for the first hidden layer
+        self.fc2 = nn.Linear(128, 6)  # Output layer (128 to 6, one for each class)
+
+        
+        
         raise NotImplementedError('MLPClassifier.__init__')
 
     def forward(self, x):
@@ -71,6 +80,12 @@ class MLPClassifier(torch.nn.Module):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         return self.fc3(x)
+
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.fc2(x)
+
+        return x
         raise NotImplementedError('MLPClassifier.forward')
 
 

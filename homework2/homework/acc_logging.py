58caa2622cee988df6753e157c3da6582e1b6ed1
MallikaUT <mallika.chaulagain@utexas.edu>
2023-09-12 04:15:53 +0000

    Homework2

--- homework2/homework/acc_logging.py
+++ homework2/homework/acc_logging.py
@@ -12,19 +12,28 @@ def test_logging(train_logger, valid_logger):
     Make sure to set global_step correctly, for epoch=0, iteration=0: global_step=0
     Call the loss 'loss', and accuracy 'accuracy' (no slash or other namespace)
     """
+    global_step = 0  # Initialize global step counter
 
     # This is a strongly simplified training loop
     for epoch in range(10):
         torch.manual_seed(epoch)
         for iteration in range(20):
             dummy_train_loss = 0.9**(epoch+iteration/20.)
+            train_logger.add_scalar('loss', dummy_train_loss, global_step=global_step)
+            global_step += 1
             dummy_train_accuracy = epoch/10. + torch.randn(10)
-            raise NotImplementedError('Log the training loss')
-        raise NotImplementedError('Log the training accuracy')
+        avg_train_accuracy = dummy_train_accuracy.mean().item()
+        train_logger.add_scalar('accuracy', avg_train_accuracy, global_step=epoch)
+ 
+       #raise NotImplementedError('Log the training loss')
+       # raise NotImplementedError('Log the training accuracy')
         torch.manual_seed(epoch)
         for iteration in range(10):
             dummy_validation_accuracy = epoch / 10. + torch.randn(10)
-        raise NotImplementedError('Log the validation accuracy')
+        avg_validation_accuracy = dummy_validation_accuracy.mean().item()
+        valid_logger.add_scalar('accuracy', avg_validation_accuracy, global_step=epoch)
+      
+        #raise NotImplementedError('Log the validation accuracy')
 
 
 if __name__ == "__main__":

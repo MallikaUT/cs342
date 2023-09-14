8ca9fc4689ab82d4c59e248964f3906ce9e1fbf6
MallikaUT <mallika.chaulagain@utexas.edu>
2023-09-14 00:03:17 +0000

    Homework2

--- homework2/homework/train.py
+++ homework2/homework/train.py
@@ -2,11 +2,20 @@ from .models import CNNClassifier, save_model
 from .utils import accuracy, load_data
 import torch
 import torch.utils.tensorboard as tb
+import torch.optim as optim
 
 
 def train(args):
     from os import path
     model = CNNClassifier()
+
+    # Define your dataset and dataloaders here using load_data() or your own dataset setup
+    train_loader, valid_loader = load_data(...)  # You need to implement load_data()
+
+    # Define loss function and optimizer
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
@@ -16,6 +25,60 @@ def train(args):
     Your code here, modify your HW1 code
     
     """
+
+    # Training loop
+    num_epochs = 10
+    global_step = 0
+
+    for epoch in range(num_epochs):
+        model.train()
+        total_correct = 0
+        total_samples = 0
+
+        for batch_data, batch_labels in train_loader:
+            optimizer.zero_grad()
+
+            # Forward pass
+            outputs = model(batch_data)
+
+            # Compute loss
+            loss = criterion(outputs, batch_labels)
+
+            # Backward pass and optimization
+            loss.backward()
+            optimizer.step()
+
+            # Compute training accuracy
+            _, predicted = torch.max(outputs, 1)
+            total_correct += (predicted == batch_labels).sum().item()
+            total_samples += batch_labels.size(0)
+
+            # Log training loss
+            train_logger.add_scalar('train/loss', loss.item(), global_step=global_step)
+            global_step += 1
+
+        # Calculate training accuracy and log
+        train_accuracy = total_correct / total_samples
+        train_logger.add_scalar('train/accuracy', train_accuracy, global_step=epoch)
+
+        # Validation loop
+        model.eval()
+        total_correct = 0
+        total_samples = 0
+
+        for batch_data, batch_labels in valid_loader:
+            # Forward pass
+            outputs = model(batch_data)
+
+            # Compute validation accuracy
+            _, predicted = torch.max(outputs, 1)
+            total_correct += (predicted == batch_labels).sum().item()
+            total_samples += batch_labels.size(0)
+
+        # Calculate validation accuracy and log
+        valid_accuracy = total_correct / total_samples
+        valid_logger.add_scalar('valid/accuracy', valid_accuracy, global_step=epoch)
+
 
     save_model(model)
 
--- homework2/homework/utils.py
+++ homework2/homework/utils.py
@@ -14,20 +14,37 @@ class SuperTuxDataset(Dataset):
         Your code here
         Hint: Use your solution (or the master solution) to HW1
         """
-        raise NotImplementedError('SuperTuxDataset.__init__')
+        self.dataset_path = dataset_path
+        self.data = [] 
+        #raise NotImplementedError('SuperTuxDataset.__init__')
 
     def __len__(self):
         """
         Your code here
         """
-        raise NotImplementedError('SuperTuxDataset.__len__')
+        return len(self.data)
+        #raise NotImplementedError('SuperTuxDataset.__len__')
 
     def __getitem__(self, idx):
         """
         Your code here
         return a tuple: img, label
         """
-        raise NotImplementedError('SuperTuxDataset.__getitem__')
+        image_path, label = self.data[idx]
+        image = Image.open(image_path).convert('RGB')  # Load image and convert to RGB
+        label = int(label)  # Ensure label is an integer
+        
+        # Implement any necessary data transformations (e.g., resizing, normalization) here
+        transform = transforms.Compose([
+            transforms.Resize((64, 64)),  # Resize the image to a fixed size
+            transforms.ToTensor(),  # Convert image to a PyTorch tensor
+            # Add more transformations as needed
+        ])
+        image = transform(image)
+
+        return image, label
+
+        #raise NotImplementedError('SuperTuxDataset.__getitem__')
 
 
 def load_data(dataset_path, num_workers=0, batch_size=128):

6f28af30b2d3b5d8f123d1dfa6e0a55071b6fc48
MallikaUT <mallika.chaulagain@utexas.edu>
2023-09-16 01:03:56 +0000

    Homework2

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -8,7 +8,6 @@ class CNNClassifier(torch.nn.Module):
         Your code here
         """
         super(CNNClassifier, self).__init__()
-                # Define the convolutional layers
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -17,11 +16,9 @@ class CNNClassifier(torch.nn.Module):
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        # Define the fully connected layers
         self.fc1 = nn.Linear(in_features=32 * 16 * 16, out_features=64)
         self.relu3 = nn.ReLU()
-        self.fc2 = nn.Linear(in_features=64, out_features=6)  # Output has 6 classes
-
+        self.fc2 = nn.Linear(in_features=64, out_features=6)  
        # raise NotImplementedError('CNNClassifier.__init__')
 
     def forward(self, x):
@@ -30,19 +27,14 @@ class CNNClassifier(torch.nn.Module):
         @x: torch.Tensor((B,3,64,64))
         @return: torch.Tensor((B,6))
         """
-         # Forward pass through convolutional layers
         x = self.conv1(x)
         x = self.relu1(x)
         x = self.pool1(x)
-
         x = self.conv2(x)
         x = self.relu2(x)
         x = self.pool2(x)
-
-        # Flatten the output for fully connected layers
         x = x.view(x.size(0), -1)
 
-        # Forward pass through fully connected layers
         x = self.fc1(x)
         x = self.relu3(x)
         x = self.fc2(x)

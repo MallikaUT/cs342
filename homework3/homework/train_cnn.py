15b9141886acb16e2fe2cec63727f232d6eed20e
MallikaUT <mallika.chaulagain@utexas.edu>
2023-09-30 18:48:03 +0000

    Homework3

--- homework3/homework/train_cnn.py
+++ homework3/homework/train_cnn.py
@@ -16,6 +16,43 @@ def train(args):
     """
     Your code here, modify your HW1 / HW2 code
     """
+
+    # Define your dataset and dataloaders
+    train_loader = load_data(args.train_data, batch_size=args.batch_size)
+    valid_loader = load_data(args.valid_data, batch_size=args.batch_size)
+
+    # Define loss function and optimizer
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    train_logger, valid_logger = None, None
+    if args.log_dir is not None:
+        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
+        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'), flush_secs=1)
+
+    # Training loop
+    for epoch in range(args.epochs):
+        model.train()
+        total_loss = 0.0
+
+        for batch_data, batch_labels in train_loader:
+            optimizer.zero_grad()
+            outputs = model(batch_data)
+            loss = criterion(outputs, batch_labels)
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        # Calculate average loss for the epoch
+        avg_loss = total_loss / len(train_loader)
+
+        # Log the training loss
+        if train_logger:
+            train_logger.add_scalar('train/loss', avg_loss, epoch)
+
+        print(f'Epoch [{epoch + 1}/{args.epochs}] - Avg. Loss: {avg_loss:.4f}')
+
     save_model(model)
 
 
@@ -26,6 +63,12 @@ if __name__ == '__main__':
 
     parser.add_argument('--log_dir')
     # Put custom arguments here
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--train_data', default='data/train')
+    parser.add_argument('--valid_data', default='data/valid')
 
     args = parser.parse_args()
     train(args)
--- homework3/homework/train_fcn.py
+++ homework3/homework/train_fcn.py
@@ -10,6 +10,11 @@ import torch.utils.tensorboard as tb
 def train(args):
     from os import path
     model = FCN()
+
+    # Define loss function and optimizer
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
@@ -22,6 +27,40 @@ def train(args):
     Hint: If you found a good data augmentation parameters for the CNN, use them here too. Use dense_transforms
     Hint: Use the log function below to debug and visualize your model
     """
+
+    # Data loading and preprocessing (replace with your dataset loading code)
+    train_loader, valid_loader = load_dense_data(args.train_data, args.valid_data, batch_size=args.batch_size)
+
+    # Training loop
+    for epoch in range(args.epochs):
+        model.train()
+        total_loss = 0.0
+        confusion_matrix = ConfusionMatrix()
+
+        for batch_data, batch_labels in train_loader:
+            optimizer.zero_grad()
+            outputs = model(batch_data)
+            loss = criterion(outputs, batch_labels)
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+
+            # Update confusion matrix and calculate IoU
+            confusion_matrix.add(outputs.argmax(1), batch_labels)
+            iou = confusion_matrix.iou()
+
+        # Calculate average loss for the epoch
+        avg_loss = total_loss / len(train_loader)
+
+        # Log training metrics
+        if train_logger:
+            train_logger.add_scalar('train/loss', avg_loss, epoch)
+            train_logger.add_scalar('train/iou', iou, epoch)
+
+        print(f'Epoch [{epoch + 1}/{args.epochs}] - Avg. Loss: {avg_loss:.4f} - IoU: {iou:.4f}')
+
+
     save_model(model)
 
 
@@ -47,6 +86,12 @@ if __name__ == '__main__':
 
     parser.add_argument('--log_dir')
     # Put custom arguments here
+    parser.add_argument('--lr', type=float, default=0.001)  # Adjust learning rate
+    parser.add_argument('--batch_size', type=int, default=32)  # Adjust batch size
+    parser.add_argument('--epochs', type=int, default=10)  # Adjust the number of training epochs
+    parser.add_argument('--train_data', default='data/train')  # Path to training data
+    parser.add_argument('--valid_data', default='data/valid')  # Path to validation data
+
 
     args = parser.parse_args()
     train(args)

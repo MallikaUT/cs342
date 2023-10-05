9cfecb34a9df964a920be2828a3a0dbaa69cd6ae
MallikaUT <mallika.chaulagain@utexas.edu>
2023-10-05 02:13:13 +0000

    Homework3

--- homework3/homework/utils.py
+++ homework3/homework/utils.py
@@ -26,78 +26,30 @@ class SuperTuxDataset(Dataset):
         Hint: Do not store torch.Tensor's as data here, but use PIL images, torchvision.transforms expects PIL images
               for most transformations.
         """
-        """self.dataset_path = dataset_path
-        self.data = [] 
-
-        label_map = {
-            'background': 0,
-            'kart': 1,
-            'pickup': 2,
-            'nitro': 3,
-            'bomb': 4,
-            'projectile': 5
-         }
- 
+        self.data = []
+        label_map = {label: idx for idx, label in enumerate(LABEL_NAMES)}
         with open(os.path.join(dataset_path, 'labels.csv'), 'r') as file:
             csv_reader = csv.reader(file)
-            next(csv_reader)  
+            next(csv_reader)
             for row in csv_reader:
                 image_path = os.path.join(dataset_path, row[0])
-                label = label_map.get(row[1], -1) 
+                label = label_map.get(row[1], -1)
                 if label != -1:
                     self.data.append((image_path, label))
-
         self.transform = transform
-        #raise NotImplementedError('SuperTuxDataset.__init__')
 
     def __len__(self):
-        
-       # Your code here
-        
         return len(self.data)
-        #raise NotImplementedError('SuperTuxDataset.__len__')
 
     def __getitem__(self, idx):
-        
-       # Your code here
-       
         image_path, label = self.data[idx]
-        image = Image.open(image_path).convert('RGB')  
-        label = int(label)  
-              
-        transform = transforms.Compose([
-            transforms.Resize((64, 64)),  
-            transforms.ToTensor(),             
-        ])
-        image = transform(image)
+        image = Image.open(image_path).convert('RGB')
+        label = int(label)
+
+        if self.transform is not None:
+            image = self.transform(image)
 
         return image, label
-        #raise NotImplementedError('SuperTuxDataset.__getitem__')
-        #return img, label
-"""
-        import csv
-        from os import path
-        self.data = []
-        to_tensor = transforms.ToTensor()
-        with open(path.join(dataset_path, 'labels.csv'), newline='') as f:
-            reader = csv.reader(f)
-            for fname, label, _ in reader:
-                if label in LABEL_NAMES:
-                    image = Image.open(path.join(dataset_path, fname))
-                    label_id = LABEL_NAMES.index(label)
-                    self.data.append((to_tensor(image), label_id))
-
-    def __len__(self):
-        """
-        Your code here
-        """
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        """
-        Your code here
-        """
-        return self.data[idx]
 
 class DenseSuperTuxDataset(Dataset):
     def __init__(self, dataset_path, transform=dense_transforms.ToTensor()):
@@ -115,8 +67,10 @@ class DenseSuperTuxDataset(Dataset):
         b = self.files[idx]
         im = Image.open(b + '_im.jpg')
         lbl = Image.open(b + '_seg.png')
+
         if self.transform is not None:
             im, lbl = self.transform(im, lbl)
+
         return im, lbl
 
 
@@ -128,6 +82,7 @@ def load_data(dataset_path, num_workers=0, batch_size=128, **kwargs):
 def load_dense_data(dataset_path, num_workers=0, batch_size=32, **kwargs):
     dataset = DenseSuperTuxDataset(dataset_path, **kwargs)
     return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=True)
+
 
 def accuracy(outputs, labels):
     outputs_idx = outputs.max(1)[1].type_as(labels)

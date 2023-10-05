c2a516237e0a5f61e4dd4023eaaa3e21df8a6b00
MallikaUT <mallika.chaulagain@utexas.edu>
2023-10-05 01:50:49 +0000

    Homework3

--- homework3/homework/models.py
+++ homework3/homework/models.py
@@ -3,14 +3,14 @@ import torch.nn.functional as F
 import torch.nn as nn
 
 
-"""class CNNClassifier(torch.nn.Module):
+class CNNClassifier(torch.nn.Module):
     def __init__(self):
         #super().__init__()
-        
+        """
         Your code here
         Hint: Base this on yours or HW2 master solution if you'd like.
         Hint: Overall model can be similar to HW2, but you likely need some architecture changes (e.g. ResNets)
-        
+        """
         super(CNNClassifier, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()
@@ -23,12 +23,12 @@ import torch.nn as nn
         #raise NotImplementedError('CNNClassifier.__init__')
 
     def forward(self, x):
-        
+        """
         Your code here
         @x: torch.Tensor((B,3,64,64))
         @return: torch.Tensor((B,6))
         Hint: Apply input normalization inside the network, to make sure it is applied in the grader
-        
+        """
         x = self.conv1(x)
         x = self.relu1(x)
         x = self.pool1(x)
@@ -42,22 +42,6 @@ import torch.nn as nn
         return x
 
        #raise NotImplementedError('CNNClassifier.forward')
-"""
-class CNNClassifier(torch.nn.Module):
-    def __init__(self, layers=[16, 32, 64, 128], n_input_channels=3, n_output_channels=6, kernel_size=5):
-        super().__init__()
-
-        L = []
-        c = n_input_channels
-        for l in layers:
-            L.append(torch.nn.Conv2d(c, l, kernel_size, stride=2, padding=kernel_size//2))
-            L.append(torch.nn.ReLU())
-            c = l
-        self.network = torch.nn.Sequential(*L)
-        self.classifier = torch.nn.Linear(c, n_output_channels)
-
-    def forward(self, x):
-        return self.classifier(self.network(x).mean(dim=[2, 3]))
 
 
 class FCN(torch.nn.Module):
@@ -117,7 +101,6 @@ def save_model(model):
     from torch import save
     from os import path
     for n, m in model_factory.items():
-    #if isinstance(model, CNNClassifier):
         if isinstance(model, m):
             return save(model.state_dict(), path.join(path.dirname(path.abspath(__file__)), '%s.th' % n))
     raise ValueError("model type '%s' not supported!" % str(type(model)))

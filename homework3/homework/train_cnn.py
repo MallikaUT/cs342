6435848cc6a28ae6880443c8600374ae83fe2add
MallikaUT <mallika.chaulagain@utexas.edu>
2023-10-05 01:58:34 +0000

    Homework3

--- homework3/homework/train_cnn.py
+++ homework3/homework/train_cnn.py
@@ -74,76 +74,106 @@ if __name__ == '__main__':
     train(args)"""
 
 
+import torch
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.transforms import functional as F
 from .models import CNNClassifier, save_model
 from .utils import accuracy, load_data
-import torch
-import torch.utils.tensorboard as tb
+from os import path
 
 def train(args):
-    from os import path
-    model = CNNClassifier()
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # Initialize the model
+    model = CNNClassifier().to(device)
+
+    # Set up TensorBoard loggers
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
         valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'))
 
-    """
-    Your code here, modify your HW1 code
-    
-    """
-    import torch
+    # Define data transformations and data loaders
+    train_transforms = transforms.Compose([
+        transforms.RandomRotation(15),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+        transforms.RandomResizedCrop(64, scale=(0.8, 1.0)),
+        transforms.ToTensor(),
+    ])
 
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    train_loader = load_data(args.train_data, batch_size=args.batch_size, transform=train_transforms)
+    valid_loader = load_data(args.valid_data, batch_size=args.batch_size)
 
-    model = CNNClassifier().to(device)
-    if args.continue_training:
-        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'cnn.th')))
+    # Define loss function and optimizer
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9)
-    loss = torch.nn.CrossEntropyLoss()
+    # Learning rate scheduler
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=args.lr_scheduler_step, gamma=args.lr_scheduler_gamma)
 
-    train_data = load_data('data/train')
-    valid_data = load_data('data/valid')
+    # Early stopping variables
+    best_valid_accuracy = 0.0
+    no_improvement_count = 0
 
-    global_step = 0
-    for epoch in range(args.num_epoch):
+    for epoch in range(args.epochs):
         model.train()
-        acc_vals = []
-        for img, label in train_data:
-            img, label = img.to(device), label.to(device)
+        total_loss = 0.0
 
-            logit = model(img)
-            loss_val = loss(logit, label)
-            acc_val = accuracy(logit, label)
-
-            if train_logger is not None:
-                train_logger.add_scalar('loss', loss_val, global_step)
-            acc_vals.append(acc_val.detach().cpu().numpy())
-
+        for batch_data, batch_labels in train_loader:
             optimizer.zero_grad()
-            loss_val.backward()
+            batch_data, batch_labels = batch_data.to(device), batch_labels.to(device)
+            outputs = model(batch_data)
+            loss = criterion(outputs, batch_labels)
+            loss.backward()
             optimizer.step()
-            global_step += 1
-        avg_acc = sum(acc_vals) / len(acc_vals)
 
+            total_loss += loss.item()
+
+        # Calculate average loss for the epoch
+        avg_loss = total_loss / len(train_loader)
+
+        # Log the training loss
         if train_logger:
-            train_logger.add_scalar('accuracy', avg_acc, global_step)
+            train_logger.add_scalar('train/loss', avg_loss, epoch)
 
+        print(f'Epoch [{epoch + 1}/{args.epochs}] - Avg. Loss: {avg_loss:.4f}')
+
+        # Learning rate scheduling step
+        scheduler.step()
+
+        # Validation loop
         model.eval()
-        acc_vals = []
-        for img, label in valid_data:
-            img, label = img.to(device), label.to(device)
-            acc_vals.append(accuracy(model(img), label).detach().cpu().numpy())
-        avg_vacc = sum(acc_vals) / len(acc_vals)
+        valid_acc_vals = []
+        for batch_data, batch_labels in valid_loader:
+            batch_data, batch_labels = batch_data.to(device), batch_labels.to(device)
+            valid_outputs = model(batch_data)
+            valid_accuracy = accuracy(valid_outputs, batch_labels).detach().cpu().numpy()
+            valid_acc_vals.append(valid_accuracy)
+
+        avg_valid_accuracy = sum(valid_acc_vals) / len(valid_acc_vals)
 
         if valid_logger:
-            valid_logger.add_scalar('accuracy', avg_vacc, global_step)
+            valid_logger.add_scalar('valid/accuracy', avg_valid_accuracy, epoch)
 
-        if valid_logger is None or train_logger is None:
-            print('epoch %-3d \t acc = %0.3f \t val acc = %0.3f' % (epoch, avg_acc, avg_vacc))
-        save_model(model)
-    save_model(model)
+        print(f'Validation Accuracy: {avg_valid_accuracy:.4f}')
 
+        # Early stopping check
+        if avg_valid_accuracy > best_valid_accuracy:
+            best_valid_accuracy = avg_valid_accuracy
+            no_improvement_count = 0
+            # Save the best model
+            save_model(model)
+        else:
+            no_improvement_count += 1
+
+        # Early stopping condition
+        if no_improvement_count >= args.early_stopping_patience:
+            print("No improvement in validation accuracy. Early stopping.")
+            break
 
 if __name__ == '__main__':
     import argparse
@@ -152,12 +182,20 @@ if __name__ == '__main__':
 
     parser.add_argument('--log_dir')
     # Put custom arguments here
-    parser.add_argument('-n', '--num_epoch', type=int, default=50)
-    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
-    parser.add_argument('-c', '--continue_training', action='store_true')
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--train_data', default='data/train')
+    parser.add_argument('--valid_data', default='data/valid')
+    parser.add_argument('--weight_decay', type=float, default=0.001)  # L2 weight regularization
+    parser.add_argument('--lr_scheduler_step', type=int, default=10)  # Learning rate scheduler step size
+    parser.add_argument('--lr_scheduler_gamma', type=float, default=0.5)  # Learning rate scheduler gamma
+    parser.add_argument('--early_stopping_patience', type=int, default=10)
 
     args = parser.parse_args()
     train(args)
+
 
 
 

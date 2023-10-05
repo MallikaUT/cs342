38a2320d096d7512819a754822b1938cd3ec559b
MallikaUT <mallika.chaulagain@utexas.edu>
2023-10-05 02:28:17 +0000

    Homework3

--- homework3/homework/utils.py
+++ homework3/homework/utils.py
@@ -64,18 +64,24 @@ class DenseSuperTuxDataset(Dataset):
 
         return im, lbl
 
+# Custom collate function
+def custom_collate_fn(batch):
+    images, labels = zip(*batch)
+    images = torch.stack(images)  # Convert PIL images to a tensor
+    return images, labels
+
 def load_data(dataset_path, num_workers=0, batch_size=128, **kwargs):
     dataset = SuperTuxDataset(dataset_path, **kwargs)
-    return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=True)
+    return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=True, collate_fn=custom_collate_fn)
 
 def load_dense_data(dataset_path, num_workers=0, batch_size=32, **kwargs):
     dataset = DenseSuperTuxDataset(dataset_path, **kwargs)
-    return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=True)
+    return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=True, collate_fn=custom_collate_fn)
 
 def accuracy(outputs, labels):
     outputs_idx = outputs.max(1)[1].type_as(labels)
     return outputs_idx.eq(labels).float().mean()
-
+    
 def _one_hot(x, n):
     return (x.view(-1, 1) == torch.arange(n, dtype=x.dtype, device=x.device)).int()
 

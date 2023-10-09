3e7b7c0db1e398a41bf46ac941451678a7678d82
MallikaUT <mallika.chaulagain@utexas.edu>
2023-10-09 02:41:49 +0000

    Homework3

--- homework3/homework/train_fcn.py
+++ homework3/homework/train_fcn.py
@@ -3,9 +3,9 @@ from os import path
 from torch.utils.data import DataLoader
 import torch.utils.tensorboard as tb
 from .models import FCN, save_model  # Assuming your FCN model and save_model function are in a file named 'models.py'
-from .utils import load_dense_data, ConfusionMatrix
+from .utils import load_dense_data, ConfusionMatrix, DenseSuperTuxDataset
 from .dense_transforms import Compose, ToTensor  # You should import your custom transformations here
-from dense_super_tux_dataset import DenseSuperTuxDataset  # Make sure you import the dataset
+#from dense_super_tux_dataset import DenseSuperTuxDataset  # Make sure you import the dataset
 
 # Define a function to calculate class weights based on class distribution
 def calculate_class_weights(class_distribution):

96db81119273f8628dddb90987ee4d5e15e88d66
MallikaUT <mallika.chaulagain@utexas.edu>
2023-10-09 20:00:42 +0000

    Homework3

--- homework3/homework/train_fcn.py
+++ homework3/homework/train_fcn.py
@@ -5,65 +5,70 @@ from .models import FCN, save_model, ClassificationLoss
 from .utils import load_dense_data, DENSE_CLASS_DISTRIBUTION, ConfusionMatrix
 from . import dense_transforms as T
 import torch.utils.tensorboard as tb
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import torch.optim as optim
+from tqdm import tqdm
 
 
 def train(args):
-    from os import path
-    model = FCN()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = FCN().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=0.016, momentum=0.92, weight_decay=1e-4)
+    criterion = torch.nn.CrossEntropyLoss()
+
+    train_trans = transforms.Compose([
+        transforms.ColorJitter(0.3, 0.3, 0.3, 0.3),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(96),
+        transforms.ToTensor()
+    ])
+    
+    train_data = load_dense_data('dense_data/train', transform=train_trans)
+    val_data = load_dense_data('dense_data/valid')
+
+    train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=32)
+
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
-        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
-        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'), flush_secs=1)
+        train_logger = tb.SummaryWriter(args.log_dir)
 
-    """
-    Your code here, modify your HW1 / HW2 code
-    Hint: Use ConfusionMatrix, ConfusionMatrix.add(logit.argmax(1), label), ConfusionMatrix.iou to compute
-          the overall IoU, where label are the batch labels, and logit are the logits of your classifier.
-    Hint: If you found a good data augmentation parameters for the CNN, use them here too. Use dense_transforms
-    Hint: Use the log function below to debug and visualize your model
-    """
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model.to(device)
-    loss_func = ClassificationLoss()
-    loss_func.to(device)
-    optim = torch.optim.SGD(model.parameters(), lr=0.016, momentum=0.92, weight_decay=1e-4)
     epochs = 30
-    
-    train_trans = T.Compose((T.ColorJitter(0.3, 0.3, 0.3, 0.3), T.RandomHorizontalFlip(), T.RandomCrop(96), T.ToTensor())) 
-    data = load_dense_data('dense_data/train', transform=train_trans)
-    val = load_dense_data('dense_data/valid')
+    best_val_accuracy = 0.0
 
     for epoch in range(epochs):
         model.train()
-        count = 0
-        total_loss = 0
-        for x, y in data:
-            x = x.to(device)
-            y = y.to(device)
-            y_pred = model(x)
-            loss = loss_func(y_pred, y.long())
-            total_loss = total_loss + loss.item()
-            count += 1
+        total_loss = 0.0
+        for images, labels in tqdm(train_loader, desc=f"Epoch {epoch}"):
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
             loss.backward()
-            optim.step()
-            optim.zero_grad()
-        print("Epoch: " + str(epoch) + ", Loss: " + str(total_loss/count))
+            optimizer.step()
+            total_loss += loss.item()
 
+        # Validation
         model.eval()
-        count = 0
-        accuracy = 0
-        for image, label in val:
-          image = image.to(device)
-          label = label.to(device)
-          pred = model(image)
-          accuracy = accuracy + (pred.argmax(1) == label).float().mean().item()
-          count += 1
-        print("Epoch: " + str(epoch) + ", Accuracy: " + str(accuracy/count))
-        if accuracy/count > 0.87:
-          print("break -> done")
-          break
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for images, labels in val_loader:
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
 
-    save_model(model)
+        val_accuracy = 100 * correct / total
+        print(f"Epoch {epoch}: Train Loss={total_loss/len(train_loader):.4f}, Validation Accuracy={val_accuracy:.2f}%")
+
+        if val_accuracy > best_val_accuracy:
+            best_val_accuracy = val_accuracy
+            save_model(model)
+
+    print("Training completed.")
 
 
 def log(logger, imgs, lbls, logits, global_step):

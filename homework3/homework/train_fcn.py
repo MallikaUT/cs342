1505033b584a14fffc97e1707f98e3fe31089174
MallikaUT <mallika.chaulagain@utexas.edu>
2023-10-09 20:59:29 +0000

    Homework3

--- homework3/homework/train_fcn.py
+++ homework3/homework/train_fcn.py
@@ -56,9 +56,8 @@ def train(args):
           pred = model(image)
           accuracy = accuracy + (pred.argmax(1) == label).float().mean().item()
           count += 1
-        print("Epoch: " + str(epoch) + ", Accuracy: " + str(accuracy/count))
+        print("Epoch[{epoch + 1}/{args.epochs}]" + str(epoch) + ", Accuracy: " + str(accuracy/count))
         if accuracy/count > 0.87:
-          print("break -> done")
           break
 
     save_model(model)

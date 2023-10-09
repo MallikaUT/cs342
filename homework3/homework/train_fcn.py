859c884d38e9cd853be843b8055077d6311519fe
MallikaUT <mallika.chaulagain@utexas.edu>
2023-10-09 20:20:44 +0000

    Homework3

--- homework3/homework/train_fcn.py
+++ homework3/homework/train_fcn.py
@@ -1,74 +1,99 @@
 import torch
 import numpy as np
-import torch.optim as optim
+
 from .models import FCN, save_model, ClassificationLoss
 from .utils import load_dense_data, DENSE_CLASS_DISTRIBUTION, ConfusionMatrix
 from . import dense_transforms as T
 import torch.utils.tensorboard as tb
-from torchvision import transforms
-from tqdm import tqdm
-from torch.utils.data import DataLoader
-
-
 
 def train(args):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = FCN().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=0.016, momentum=0.92, weight_decay=1e-4)
-    criterion = torch.nn.CrossEntropyLoss()
+    from os import path
 
+    # Initialize your FCN model
+    model = FCN()
+
+    # Initialize TensorBoard loggers
+    train_logger, valid_logger = None, None
+    if args.log_dir is not None:
+        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
+        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'), flush_secs=1)
+
+    # Set the device (GPU if available, otherwise CPU)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+
+    # Define loss function and optimizer
+    loss_func = ClassificationLoss()
+    loss_func.to(device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.016, momentum=0.92, weight_decay=1e-4)
+
+    # Number of training epochs
+    epochs = 30
+
+    # Define data augmentation transformations
     train_trans = transforms.Compose([
         transforms.ColorJitter(0.3, 0.3, 0.3, 0.3),
         transforms.RandomHorizontalFlip(),
         transforms.RandomCrop(96),
         transforms.ToTensor()
     ])
-    
+
+    # Load training and validation data
     train_data = load_dense_data('dense_data/train', transform=train_trans)
     val_data = load_dense_data('dense_data/valid')
 
-    train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
-    val_loader = DataLoader(val_data, batch_size=32)
-
-    train_logger, valid_logger = None, None
-    if args.log_dir is not None:
-        train_logger = tb.SummaryWriter(args.log_dir)
-
-    epochs = 30
-    best_val_accuracy = 0.0
-
     for epoch in range(epochs):
+        # Set the model to training mode
         model.train()
-        total_loss = 0.0
-        for images, labels in train_loader:
-            images, labels = images.to(device), labels.to(device)
+        count = 0
+        total_loss = 0
+
+        for x, y in train_data:
+            x = x.to(device)
+            y = y.to(device)
+
+            # Forward pass
+            y_pred = model(x)
+
+            # Calculate the loss
+            loss = loss_func(y_pred, y.long())
+            total_loss += loss.item()
+            count += 1
+
+            # Backpropagation and optimization
             optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-            total_loss += loss.item()
 
-        # Validation
+        # Print training loss for this epoch
+        print(f"Epoch: {epoch}, Loss: {total_loss / count:.4f}")
+
+        # Set the model to evaluation mode
         model.eval()
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for images, labels in val_loader:
-                images, labels = images.to(device), labels.to(device)
-                outputs = model(images)
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+        count = 0
+        accuracy = 0
 
-        val_accuracy = 100 * correct / total
-        print(f"Epoch {epoch}: Train Loss={total_loss/len(train_loader):.4f}, Validation Accuracy={val_accuracy:.2f}%")
+        for image, label in val_data:
+            image = image.to(device)
+            label = label.to(device)
 
-        if val_accuracy > best_val_accuracy:
-            best_val_accuracy = val_accuracy
-            save_model(model)
+            # Forward pass for validation
+            pred = model(image)
 
-    print("Training completed.")
+            # Calculate accuracy
+            accuracy += (pred.argmax(1) == label).float().mean().item()
+            count += 1
+
+        # Print validation accuracy for this epoch
+        print(f"Epoch: {epoch}, Accuracy: {accuracy / count:.4f}")
+
+        # Check if the desired accuracy is reached and break if so
+        if accuracy / count > 0.87:
+            print("Accuracy threshold reached. Training completed.")
+            break
+
+    # Save the trained model
+    save_model(model)
 
 
 def log(logger, imgs, lbls, logits, global_step):

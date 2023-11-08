67060669289e548c092c6a300df0e58f6695471c
MallikaUT <mallika.chaulagain@utexas.edu>
2023-11-08 16:42:58 +0000

    Homework5

--- homework5/homework/train.py
+++ homework5/homework/train.py
@@ -1,59 +1,90 @@
-from .planner import Planner, save_model 
+from planner import Planner, save_model
 import torch
 import torch.utils.tensorboard as tb
 import numpy as np
-from .utils import load_data
-from .import dense_transforms
-from torchvision import transforms
+from utils import load_data
+import dense_transforms
 
 
 def train(args):
     from os import path
+    print("Initializing Model =================================")
     model = Planner()
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
-        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
-        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'), flush_secs=1)
+        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
+
+    """
+    Your code here, modify your HW4 code
     
+    """
+    print("installing torch ...")
+    import torch
+
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    model = Planner().to(device)
-    """
-    Your code here, modify your HW3 code
-    Hint: Use the log function below to debug and visualize your model
-    """
-    loss = torch.nn.BCEWithLogitsLoss(reduction='none').to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=1e-6)
 
-    train_data = load_data('drive_data', num_workers=4)
+    model = model.to(device)
+    if args.continue_training:
+        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'planner.th')))
 
-    for epoch in range(50):
+    loss = torch.nn.L1Loss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    
+    print("installing inspect ...")
+    import inspect
+    transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
+    print("loading data ...")
+    train_data = load_data('drive_data', transform=transform, num_workers=args.num_workers)
+
+    global_step = 0
+    print("Begin Training =================================")
+    for epoch in range(args.num_epoch):
         model.train()
-        for image, heatmap, delta in train_data:
-            image = image.to(device)
-            heatmap = heatmap.to(device)
+        losses = []
+        for img, label in train_data:
+            img, label = img.to(device), label.to(device)
 
-            pred_heatmap = model(image)
+            pred = model(img)
+            loss_val = loss(pred, label)
 
-            l = loss(pred_heatmap, heatmap).mean()
-            l.backward()
-            optimizer.step()
+            if train_logger is not None:
+                train_logger.add_scalar('loss', loss_val, global_step)
+                if global_step % 100 == 0:
+                    log(train_logger, img, label, pred, global_step)
+
             optimizer.zero_grad()
-        model.eval()
-    #raise NotImplementedError('train')
+            loss_val.backward()
+            optimizer.step()
+            global_step += 1
+            
+            losses.append(loss_val.detach().cpu().numpy())
+        
+        avg_loss = np.mean(losses)
+        if train_logger is None:
+            print('epoch %-3d \t loss = %0.3f' % (epoch, avg_loss))
+        save_model(model)
+
     save_model(model)
 
 
-def log(logger, imgs, gt_det, det, global_step):
+def log(logger, img, label, pred, global_step):
     """
     logger: train_logger/valid_logger
-    imgs: image tensor from data loader
-    gt_det: ground-truth object-center maps
-    det: predicted object-center heatmaps
+    img: image tensor from data loader
+    label: ground-truth aim point
+    pred: predited aim point
     global_step: iteration
     """
-    logger.add_images('image', imgs[:16], global_step)
-    logger.add_images('label', gt_det[:16], global_step)
-    logger.add_images('pred', torch.sigmoid(det[:16]), global_step)
+    import matplotlib.pyplot as plt
+    import torchvision.transforms.functional as TF
+    fig, ax = plt.subplots(1, 1)
+    ax.imshow(TF.to_pil_image(img[0].cpu()))
+    WH2 = np.array([img.size(-1), img.size(-2)]) / 2
+    ax.add_artist(plt.Circle(WH2 * (label[0].cpu().detach().numpy() + 1), 2, ec='g', fill=False, lw=1.5))
+    ax.add_artist(plt.Circle(WH2 * (pred[0].cpu().detach().numpy() + 1), 2, ec='r', fill=False, lw=1.5))
+    logger.add_figure('viz', fig, global_step)
+    del ax, fig
+
 
 if __name__ == '__main__':
     import argparse
@@ -62,12 +93,6 @@ if __name__ == '__main__':
 
     parser.add_argument('--log_dir')
     # Put custom arguments here
-    parser.add_argument('-n', '--num_epoch', type=int, default=20)
-    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
-    parser.add_argument('-g', '--gamma', type=float, default=0, help="class dependent weight for cross entropy")
-    parser.add_argument('-c', '--continue_training', action='store_true')
-    parser.add_argument('-t', '--transform',
-                        default='Compose([ColorJitter(0.9, 0.9, 0.9, 0.1), RandomHorizontalFlip(), ToTensor()])')
 
     args = parser.parse_args()
     train(args)

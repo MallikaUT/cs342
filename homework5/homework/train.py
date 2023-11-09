88a483b78867303fa24d6b24fa177d8d2acc6977
MallikaUT <mallika.chaulagain@utexas.edu>
2023-11-09 17:54:54 +0000

    Homework5

--- homework5/homework/train.py
+++ homework5/homework/train.py
@@ -1,55 +1,82 @@
-from .planner import Planner, save_model
+from planner import Planner, save_model
 import torch
 import torch.utils.tensorboard as tb
-from .utils import load_data
-from .import dense_transforms
-import matplotlib.pyplot as plt
-import torchvision.transforms.functional as TF
 import numpy as np
+from utils import load_data
+import dense_transforms
+
 
 def train(args):
     from os import path
-    model = Planner().to(args.device)  # Move the model to the specified device
+    print("Initializing Model =================================")
+    model = Planner()
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
 
-    # Define your loss function and optimizer
-    loss_fn = torch.nn.MSELoss()
+    """
+    Your code here, modify your HW4 code
+    
+    """
+    print("installing torch ...")
+    import torch
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    model = model.to(device)
+    if args.continue_training:
+        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'planner.th')))
+
+    loss = torch.nn.L1Loss()
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    
+    print("installing inspect ...")
+    import inspect
+    transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
+    print("loading data ...")
+    train_data = load_data('drive_data', transform=transform, num_workers=args.num_workers)
 
-    # Load your dataset using load_data function
-    train_data = load_data(args.dataset_path, transform=args.transform, num_workers=args.num_workers, batch_size=args.batch_size)
-
-    # Training loop
-    for epoch in range(args.num_epochs):
+    global_step = 0
+    print("Begin Training =================================")
+    for epoch in range(args.num_epoch):
         model.train()
-        for i, data in enumerate(train_data):
-            image, label = data
-            image = image.to(args.device)  # Move the input data to the device
-            label = label.to(args.device)  # Move the label data to the device
+        losses = []
+        for img, label in train_data:
+            img, label = img.to(device), label.to(device)
 
-            # Forward pass
-            output = model(image)
+            pred = model(img)
+            loss_val = loss(pred, label)
 
-            # Compute the loss
-            loss = loss_fn(output, label)
-
-            # Backpropagation
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            # Log the visualization
             if train_logger is not None:
-                log(train_logger, image, label, output, epoch * len(train_data) + i)
+                train_logger.add_scalar('loss', loss_val, global_step)
+                if global_step % 100 == 0:
+                    log(train_logger, img, label, pred, global_step)
 
-        # Save the model after each epoch
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+            global_step += 1
+            
+            losses.append(loss_val.detach().cpu().numpy())
+        
+        avg_loss = np.mean(losses)
+        if train_logger is None:
+            print('epoch %-3d \t loss = %0.3f' % (epoch, avg_loss))
         save_model(model)
 
-    # Optionally, you can also log other training statistics using train_logger
+    save_model(model)
+
 
 def log(logger, img, label, pred, global_step):
+    """
+    logger: train_logger/valid_logger
+    img: image tensor from data loader
+    label: ground-truth aim point
+    pred: predited aim point
+    global_step: iteration
+    """
+    import matplotlib.pyplot as plt
+    import torchvision.transforms.functional as TF
     fig, ax = plt.subplots(1, 1)
     ax.imshow(TF.to_pil_image(img[0].cpu()))
     WH2 = np.array([img.size(-1), img.size(-2)]) / 2
@@ -58,19 +85,14 @@ def log(logger, img, label, pred, global_step):
     logger.add_figure('viz', fig, global_step)
     del ax, fig
 
+
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--log_dir')
-    parser.add_argument('--num_epochs', type=int, default=20)  # Add custom arguments
-    parser.add_argument('--learning_rate', type=float, default=0.001)
-    parser.add_argument('--batch_size', type=int, default=32)  # Add batch size argument
-    parser.add_argument('--dataset_path', default='drive_data', type=str)
-    parser.add_argument('--transform', default=dense_transforms.ToTensor(), type=eval)
-    parser.add_argument('--num_workers', type=int, default=4)
+    # Put custom arguments here
 
     args = parser.parse_args()
-    args.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     train(args)

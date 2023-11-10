4a217646636999f150c3c42ad3f4a5bd67d578be
MallikaUT <mallika.chaulagain@utexas.edu>
2023-11-10 15:24:55 +0000

    Homework5

--- homework5/homework/planner.py
+++ homework5/homework/planner.py
@@ -1,60 +1,35 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.models import MobileNetV2
 
-def spatial_argmax(logit):
-    """
-    Compute the soft-argmax of a heatmap
-    :param logit: A tensor of size BS x H x W
-    :return: A tensor of size BS x 2 the soft-argmax in normalized coordinates (-1 .. 1)
-    """
-    weights = F.softmax(logit.view(logit.size(0), -1), dim=-1).view_as(logit)
-    return torch.stack(((weights.sum(1) * torch.linspace(-1, 1, logit.size(2)).to(logit.device)[None]).sum(1),
-                        (weights.sum(2) * torch.linspace(-1, 1, logit.size(1)).to(logit.device)[None]).sum(1)), 1)
-
-class Planner(torch.nn.Module):
+class Planner(nn.Module):
     def __init__(self):
-        super().__init__()
-
-        layers = []
-        ic = 3
-        structure = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
-        for l in structure:
-            if l == 'M':
-                layers.append(torch.nn.MaxPool2d(kernel_size=2, stride=2))
-            else:
-                layers.append(torch.nn.Conv2d(in_channels=ic, out_channels=l, padding=1, kernel_size=3, stride=1))
-                layers.append(torch.nn.BatchNorm2d(l))
-                layers.append(torch.nn.ReLU(inplace=True))
-                ic = l
-
-        layers.append(torch.nn.Conv2d(in_channels=512, out_channels=256, kernel_size=3, padding=1))
-        layers.append(torch.nn.ReLU(inplace=True))
-        layers.append(torch.nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1))
-        layers.append(torch.nn.ReLU(inplace=True))
-        layers.append(torch.nn.Conv2d(128, 1, 1))
-
-        self._conv = torch.nn.Sequential(*layers)
-
-    def forward(self, img):
+        super(Planner, self).__init()
+        # Use a lightweight base model, such as MobileNetV2
+        self.base_model = MobileNetV2(features_only=True)
         
-        x = self._conv(img)
-        return spatial_argmax(x[:, 0])
-
+        # Additional layers to adapt the model for your task
+        self.conv1 = nn.Conv2d(1280, 256, kernel_size=1)
+        self.conv2 = nn.Conv2d(256, 128, kernel_size=1)
+        self.conv3 = nn.Conv2d(128, 1, kernel_size=1)
+        
+    def forward(self, x):
+        x = self.base_model(x)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.conv3(x)
+        return x
 
 def save_model(model):
-    from torch import save
-    from os import path
-    if isinstance(model, Planner):
-        save(model.state_dict(), path.join(path.dirname(path.abspath(__file__)), 'planner.th'))
-    else:
-        raise ValueError("Model type '%s' not supported!" % str(type(model)))
+    # Save the model using torch.save as before
+    torch.save(model.state_dict(), 'planner.th')
 
 def load_model():
-    from torch import load
-    from os import path
-    r = Planner()
-    r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), 'planner.th'), map_location='cpu'))
-    return r
+    # Load the model using torch.load as before
+    model = Planner()
+    model.load_state_dict(torch.load('planner.th'))
+    return model
 
 if __name__ == '__main__':
     from .controller import control

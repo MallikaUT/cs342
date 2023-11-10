0ae7e8eb74e82126500a6e4638ed16725803102e
MallikaUT <mallika.chaulagain@utexas.edu>
2023-11-10 16:29:58 +0000

    Homework5

--- homework5/homework/planner.py
+++ homework5/homework/planner.py
@@ -16,14 +16,16 @@ class Planner(nn.Module):
     def __init__(self):
         super(Planner, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(32 * 48 * 64, 32)  # Reduce the number of output features
-        self.fc2 = nn.Linear(32, 2)  # Output 2 channels for aim point prediction
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32 * 48 * 64, 64)
+        self.fc2 = nn.Linear(64, 2)
 
     def forward(self, img):
-        x = self.conv1(img)
+        x = F.relu(self.pool(self.conv1(img)))
         x = x.view(-1, 32 * 48 * 64)
-        x = self.fc1(x)
-        return self.fc2(x) 
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
 
 def save_model(model):

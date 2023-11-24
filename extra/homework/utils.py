f4e02ea2d5f2cd3007a53baa04d4113b7928bdb1
MallikaUT <mallika.chaulagain@utexas.edu>
2023-11-24 13:11:44 +0000

    ExtraHomework

--- extra/homework/utils.py
+++ extra/homework/utils.py
@@ -23,6 +23,9 @@ def sample_from_distribution(log_probs):
     sampled_index = torch.multinomial(probs, 1).item()
     return vocab[sampled_index]
 
+def index_to_char(index):
+    return vocab[index]
+
 class SpeechDataset(Dataset):
     """
     Creates a dataset of strings from a text file.

ca6eec7a8781c7b5a90cb84c12689eb4309fccbb
MallikaUT <mallika.chaulagain@utexas.edu>
2023-11-26 04:26:03 +0000

    Final

--- final/image_agent/player.py
+++ final/image_agent/player.py
@@ -1,3 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FCNModel(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(FCNModel, self).__init__()
+
+        # Define the encoder (downsampling) part of the U-Net
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        # Define the decoder (upsampling) part of the U-Net
+        self.decoder = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, out_channels, kernel_size=3, padding=1),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        )
+
+    def forward(self, x):
+        # Forward pass through the encoder
+        x = self.encoder(x)
+
+        # Forward pass through the decoder
+        x = self.decoder(x)
+
+        return x
 
 class Team:
     agent_type = 'image'

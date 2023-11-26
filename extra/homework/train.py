db9e0725cddb2d14323e20a8c6a3d18e0dad2251
MallikaUT <mallika.chaulagain@utexas.edu>
2023-11-26 23:43:25 +0000

    Extra

--- extra/homework/train.py
+++ extra/homework/train.py
@@ -1,34 +1,76 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
 from .models import TCN, save_model
 from .utils import SpeechDataset, one_hot
-
 
 def train(args):
     from os import path
     import torch.utils.tensorboard as tb
+
+    # Instantiate your TCN model
     model = TCN()
+
+    # Define your optimizer and loss function
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    criterion = nn.CrossEntropyLoss()
+
+    # Assuming you have training and validation datasets
+    train_dataset = SpeechDataset('path_to_train_dataset')
+    valid_dataset = SpeechDataset('path_to_valid_dataset')
+
+    # Define data loaders
+    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=32, shuffle=False)
+
+    # TensorBoard logging
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
         valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'), flush_secs=1)
 
-    """
-    Your code here, modify your code from prior assignments
-    Hint: SGD might need a fairly high learning rate to work well here
+    # Training loop
+    num_epochs = 10
+    for epoch in range(num_epochs):
+        model.train()
+        for batch in train_dataloader:
+            optimizer.zero_grad()
 
-    """
-    raise NotImplementedError('train')
+            # Assuming batch is a tensor containing your input data
+            output = model(batch)
+
+            # Assuming labels is a tensor containing your target data
+            loss = criterion(output, labels)
+
+            loss.backward()
+            optimizer.step()
+
+        # Validation loop
+        model.eval()
+        with torch.no_grad():
+            for batch in valid_dataloader:
+                # Assuming batch is a tensor containing your input data
+                output = model(batch)
+
+                # Assuming labels is a tensor containing your target data
+                loss = criterion(output, labels)
+
+        # Log training and validation losses to TensorBoard
+        if train_logger is not None:
+            train_logger.add_scalar('loss', loss.item(), epoch)
+        if valid_logger is not None:
+            valid_logger.add_scalar('loss', loss.item(), epoch)
+
+    # Save the trained model
     save_model(model)
-
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-
     parser.add_argument('--log_dir')
-    # Put custom arguments here
+    # Add any other custom arguments here
 
     args = parser.parse_args()
     train(args)

44198782972fd8a5bb86b40af80c585c710fbc63
MallikaUT <mallika.chaulagain@utexas.edu>
2023-11-26 03:45:19 +0000

    Final

--- final/image_agent/controller.py
+++ final/image_agent/controller.py
@@ -1,6 +1,5 @@
 import pystk
 
-
 def control(aim_point, current_vel):
     """
     Set the Action for the low-level controller
@@ -10,32 +9,51 @@ def control(aim_point, current_vel):
     """
     action = pystk.Action()
 
-    """
-    Your code here
-    Hint: Use action.acceleration (0..1) to change the velocity. Try targeting a target_velocity (e.g. 20).
-    Hint: Use action.brake to True/False to brake (optionally)
-    Hint: Use action.steer to turn the kart towards the aim_point, clip the steer angle to -1..1
-    Hint: You may want to use action.drift=True for wide turns (it will turn faster)
-    """
+    if current_vel <= 25:
+        action.acceleration = 1
+    elif current_vel >= 25:
+        action.acceleration = 0.2
+
+    if current_vel >= 25:
+        action.brake = True
+    else:
+        action.brake = False
+
+    action.steer = aim_point[0] * 7
+
+    if action.steer > 1:
+        action.steer = 1
+
+    if action.steer < -1:
+        action.steer = -1
+
+    if aim_point[0] <= 0.2 or aim_point[0] >= -0.2:
+        action.nitro = True
+    else:
+        action.nitro = False
+
+    if aim_point[0] <= -0.3 or aim_point[0] >= 0.3:
+        action.drift = True
+        action.acceleration = 0.8
+    else:
+        action.drift = False
+        action.acceleration = 0.8
 
     return action
-
 
 if __name__ == '__main__':
     from .utils import PyTux
     from argparse import ArgumentParser
 
     def test_controller(args):
-        import numpy as np
         pytux = PyTux()
         for t in args.track:
             steps, how_far = pytux.rollout(t, control, max_frames=1000, verbose=args.verbose)
-            print(steps, how_far)
+            print(f"Track: {t}, Steps: {steps}, Distance: {how_far}")
         pytux.close()
 
-
     parser = ArgumentParser()
-    parser.add_argument('track', nargs='+')
-    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('track', nargs='+', help='List of tracks to test the controller')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode')
     args = parser.parse_args()
     test_controller(args)

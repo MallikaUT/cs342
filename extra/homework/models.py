df307dff628904c55f9916f9bcfa3ec30277ca77
MallikaUT <mallika.chaulagain@utexas.edu>
2023-11-27 23:23:35 +0000

    extra

--- extra/homework/models.py
+++ extra/homework/models.py
@@ -4,9 +4,7 @@ import torch.nn.functional as F
 from . import utils
 from .utils import one_hot
 
-
-#NOTE THIS IS JUST TO TEST THE GRADER, NOV 15 2021
-
+# NOTE: THIS IS JUST TO TEST THE GRADER, NOV 15 2021
 
 class LanguageModel(object):
     
@@ -24,80 +22,60 @@ class LanguageModel(object):
         """
         return self.predict_all(some_text)[:, -1]
 
-
 class Bigram(LanguageModel):
  
     def __init__(self):
         from os import path
         self.first, self.transition = torch.load(path.join(path.dirname(path.abspath(__file__)), 'bigram.th'))
 
-
-    #bigram predictALL
-
     def predict_all(self, some_text):
         return torch.cat((self.first[:, None], self.transition.t().matmul(utils.one_hot(some_text))), dim=1)
-
 
 class AdjacentLanguageModel(LanguageModel):
  
     def predict_all(self, some_text):
-        prob = 1e-3*torch.ones(len(utils.vocab), len(some_text)+1)
+        prob = 1e-3 * torch.ones(len(utils.vocab), len(some_text) + 1)
         if len(some_text):
             one_hot = utils.one_hot(some_text)
-            prob[-1, 1:] += 0.5*one_hot[0]
-            prob[:-1, 1:] += 0.5*one_hot[1:]
-            prob[0, 1:] += 0.5*one_hot[-1]
-            prob[1:, 1:] += 0.5*one_hot[:-1]
-        return (prob/prob.sum(dim=0, keepdim=True)).log()
+            prob[-1, 1:] += 0.5 * one_hot[0]
+            prob[:-1, 1:] += 0.5 * one_hot[1:]
+            prob[0, 1:] += 0.5 * one_hot[-1]
+            prob[1:, 1:] += 0.5 * one_hot[:-1]
+        return (prob / prob.sum(dim=0, keepdim=True)).log()
 
-#==============================================TCN===========================================
-
-
-
-class TCN(torch.nn.Module, LanguageModel):     #MY WARNING:  TCN in example DOES NOT inherit from Language Model
+class TCN(torch.nn.Module, LanguageModel):
     class CausalConv1dBlock(torch.nn.Module):
-        
-        
         def __init__(self, in_channels, out_channels, kernel_size, dilation):
-          
-            self.pad1d = torch.nn.ConstantPad1d((2*dilation,0), 0)
+            super().__init__()
+            self.pad1d = torch.nn.ConstantPad1d((2 * dilation, 0), 0)
             self.c1 = torch.nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size // 2, dilation=dilation)
-            #self.b1 = torch.nn.BatchNorm2d(out_channels)
-                          
-         
+
         def forward(self, x):
             return F.relu(self.c1(self.pad1d(x))) 
-    
-    #--------------->TCN INIT
 
-    def __init__(self, layers=[28,16,8], char_set="string"):
+    def __init__(self, layers=[28, 16, 8], char_set="string"):
         super().__init__()
 
-        total_dilation = 1  # starting dilation at 2, not 1?????
+        total_dilation = 1
         c = 28
         L = []
 
         for l in layers:
-            
             L.append(torch.nn.ConstantPad1d((total_dilation, 0), 0))
             L.append(torch.nn.Conv1d(c, l, kernel_size=2, dilation=total_dilation))
             L.append(torch.nn.ReLU())
             total_dilation *= 2
             c = l
 
-        self.total_dilation = total_dilation  # Move this line here
+        self.total_dilation = total_dilation
         self.network = torch.nn.Sequential(*L)
         self.classifier = torch.nn.Conv1d(c, 28, 1)
-        
-    #--------------------------TCN FORWARD()
-    
+
     def forward(self, x):
         print("Input sequence size:", x.size())
 
-        # Check if the input sequence is too short
         if x.size(2) < 3:
-            # Handle short sequences, for example, return a default value
-            return torch.zeros(x.size(0), 28, 1)  # Modify the shape as needed
+            return torch.zeros(x.size(0), 28, 1)
 
         first_char_distribution = torch.nn.Parameter(torch.rand(x.size(0), x.size(1), 1))
         total_dilation = self.total_dilation
@@ -106,7 +84,7 @@ class TCN(torch.nn.Module, LanguageModel):     #MY WARNING:  TCN in example DOES
         output = torch.cat((first_char_distribution, output), dim=2)
 
         return output
-        
+
     def predict_all(self, some_text):
         one_hotx = one_hot(some_text)
         one_hotx = one_hotx.unsqueeze(0)
@@ -117,27 +95,17 @@ class TCN(torch.nn.Module, LanguageModel):     #MY WARNING:  TCN in example DOES
 
         output = self.forward(one_hotx)
 
-        # Ensure that the output has the correct shape (28, len(some_text) + 1)
-        if output.size(2) != len(some_text) + 1:
-            # If not, pad or truncate the output to the correct length
-            output = F.pad(output, (0, len(some_text) + 1 - output.size(2)))
+        if output.size(1) != len(utils.vocab) or output.size(2) != len(some_text) + 1:
+            output = output.permute(0, 2, 1)
 
-        # Apply log_softmax along the sequence dimension (dim=2)
         output = F.log_softmax(output, dim=2)
-
-        # Sum along the vocabulary dimension (dim=1) to get log probabilities for each position
         log_probs = output.sum(dim=1)
 
         return log_probs.squeeze()
 
-
-            
-        
-
 def save_model(model):
     from os import path
     return torch.save(model.state_dict(), path.join(path.dirname(path.abspath(__file__)), 'tcn.th'))
-
 
 def load_model():
     from os import path

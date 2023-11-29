527493f3eacf483380d0a48cb79999e97f279737
MallikaUT <mallika.chaulagain@utexas.edu>
2023-11-29 06:24:58 +0000

    FinalProject

--- final/image_agent/controller.py
+++ final/image_agent/controller.py
@@ -1,102 +1,33 @@
-import pystk
 import numpy as np
 
+def control(aim_point, player_state):
+    """
+    Set the Action for the low-level controller
+    :param aim_point: Aim point, in screen coordinate frame [-1..1]
+    :param current_vel: Current velocity of the kart
+    :return: a pystk.Action (set acceleration, brake, steer, drift)
+    """
 
-def control(aim_point, current_vel):
-
-    action = pystk.Action()
-
-    #Steering and relative aim point use different units. Use the aim point 
-    #and a tuned scaling factor to select the amount of normalized steering.
+    """
+    Your code here
+    Hint: Use action.acceleration (0..1) to change the velocity. Try targeting a target_velocity (e.g. 20).
+    Hint: Use action.brake to True/False to brake (optionally)
+    Hint: Use action.steer to turn the kart towards the aim_point, clip the steer angle to -1..1
+    Hint: You may want to use action.drift=True for wide turns (it will turn faster)
+    """
+    current_vel=np.linalg.norm(player_state['kart']['velocity'])
+    target_vel=20
+    steer_factor=2.21
+    drift_thresh=0.65
+    brake_factor=0.75
     
-    steer_dir = aim_point[0]
+    acceleration = target_vel - current_vel
+    acceleration = np.clip(acceleration, 0, 1)
 
-    action.steer = steer_dir  
-    action.brake = False
-    action.nitro = True
-    action.acceleration = 1
+    steer = np.clip(aim_point[0][0] * steer_factor, -1, 1)
+
+    brake = steer > brake_factor
     
-    if aim_point[1] < 0:
-      t=1
-      action.nitro = True
-      #print ("Facing Front")
-
-    if aim_point[1] > 0:
-      t=1
-      action.acceleration = 0
-      #print ("LOOK BEHIND YOU")
-
-    if abs(steer_dir)<.2:
-      action.nitro = True         #NITROOOOOOOO
-      #print("NITRO")
-
-    #if abs(aim_point[0])<.2:
-     # action.acceleration = 100
-
-    #print (current_vel)
-    if abs(steer_dir)<=.5:
-      action.drift = False
+    drift = abs(steer) > drift_thresh
     
-    #comment 
-    
-    direction_steer = np.sign(aim_point[0])
-
-    
-
-    #print (aim_point[0])
-
-    if steer_dir > 1:
-      steer_dir = 1
-
-    if steer_dir < -1:
-      steer_dir = -1
-    
-
-    if abs(steer_dir)>.9:
-      action.steer = (abs(steer_dir)*direction_steer)
-      action.acceleration = 0
-      action.acceleration = False
-      #action.nitro = False
-
-    if (abs(steer_dir)>.45) and (abs(aim_point[0])<=.7):
-      action.drift = True
-      action.acceleration = 0.1  #.1
-
-    if (abs(steer_dir)>.7):
-      #print (f'SLIGHT tight curve ahead, speed is {current_vel}, steering at {aim_point[0]}, acceleration {action.acceleration}, brake {action.brake}')
-      action.drift = True
-      action.acceleration = 0.00
-      #action.nitro = False 
-      
-   
-    if (abs(steer_dir)>.7) and current_vel > 15:
-      action.brake = True 
-      action.nitro = False
-      #print (f'tight curve ahead, speed is {current_vel}, steering at {aim_point[0]}, acceleration {action.acceleration}, brake {action.brake}')
-
-    if current_vel > 23:    #24
-      action.brake = True
-      #action.nitro = False
-
-    
-    return action
-
-
-if __name__ == '__main__':
-    from .utils import PyTux
-    from argparse import ArgumentParser
-
-    def test_controller(args):
-        import numpy as np
-        pytux = PyTux()
-        for t in args.track:
-            steps, how_far = pytux.rollout(t, control, max_frames=1000, verbose=args.verbose)
-            print(steps, how_far)
-        pytux.close()
-
-
-    parser = ArgumentParser()
-    parser.add_argument('track', nargs='+')
-    parser.add_argument('-v', '--verbose', action='store_true')
-    args = parser.parse_args()
-    test_controller(args)
+    return acceleration, steer, brake

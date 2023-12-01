f001e2955fa5ede503df4f379a01f286e92cd9ce
MallikaUT <mallika.chaulagain@utexas.edu>
2023-12-01 04:31:51 +0000

    FinalProject

--- final/image_agent/train.py
+++ final/image_agent/train.py
@@ -2,7 +2,7 @@ from .planner import Planner, save_model
 import torch
 import torch.utils.tensorboard as tb
 import numpy as np
-from .utils import load_data, custom_collate
+from .utils import load_data
 from . import dense_transforms
 import torch.nn.functional as F
 from torchvision.transforms import Resize

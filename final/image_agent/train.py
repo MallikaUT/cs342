ecce64069a6d8b83468ff6526522e9fb764f55a1
MallikaUT <mallika.chaulagain@utexas.edu>
2023-12-01 04:58:22 +0000

    FinalProject

--- final/image_agent/train.py
+++ final/image_agent/train.py
@@ -3,95 +3,59 @@ import torch
 import torch.utils.tensorboard as tb
 import numpy as np
 from .utils import load_data
-from . import dense_transforms as DT
-import torch.nn.functional as F
-from torchvision.transforms import Resize
-
-
-
-def custom_collate_fn(batch):
-    images, labels = zip(*batch)
-    
-    # Pad labels to the maximum size in the batch
-    max_size = max(label.size(0) for label in labels)
-    labels = [F.pad(label, (0, max_size - label.size(0)), value=-1) for label in labels]
-    
-    return torch.stack(images), torch.stack(labels)
+from . import dense_transforms
 
 def train(args):
     from os import path
-    print("Initializing Model =================================")
     model = Planner()
-    train_logger, valid_logger = None, None
-    if args.log_dir is not None:
-        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
 
-    import torch
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    """
+    Your code here, modify your HW4 code
+    Hint: Use the log function below to debug and visualize your model
+    """
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
-    loss = torch.nn.L1Loss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    
+    if args.continue_training:
+        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'planner.th')))
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=1e-5)
+    #w = torch.as_tensor(DENSE_CLASS_DISTRIBUTION)**(-args.gamma)
+    loss = torch.nn.MSELoss()  #(weight=w / w.mean()).to(device)
 
-    transform = DT.Compose([
-        DT.ColorJitter(brightness=0.2, contrast=0.5, saturation=0.5, hue=0.2),
-        DT.RandomHorizontalFlip(),
-        DT.ToTensor()
-    ])
-
-    print("Loading data ...")
-    train_data = load_data(transform=transform, num_workers=args.num_workers)
+    import inspect
+    transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
+    train_data = load_data(num_workers=4, transform=transform)
 
     global_step = 0
-    print("Begin Training =================================")
     for epoch in range(args.num_epoch):
         model.train()
-        losses = []
+        losst = 0
         for img, label in train_data:
             img, label = img.to(device), label.to(device)
 
-            pred = model(img)
-            loss_val = loss(pred, label)
-
-            if train_logger is not None:
-                train_logger.add_scalar('loss', loss_val, global_step)
-                if global_step % 100 == 0:
-                    log(train_logger, img, label, pred, global_step)
-
+            logit = model(img)
+            loss_val = loss(logit, label)
+            losst += loss_val
             optimizer.zero_grad()
             loss_val.backward()
             optimizer.step()
             global_step += 1
-            
-            losses.append(loss_val.detach().cpu().numpy())
-        
-        avg_loss = np.mean(losses)
-        if train_logger is None:
-            print('epoch %-3d \t loss = %0.3f' % (epoch, avg_loss))
-        save_model(model)
+        print('\nEpoch: ' + str(epoch + 1) + " Loss: " + str(losst))
 
     save_model(model)
-
-def log(logger, img, label, pred, global_step):
-    import matplotlib.pyplot as plt
-    import torchvision.transforms.functional as TF
-    fig, ax = plt.subplots(1, 1)
-    ax.imshow(TF.to_pil_image(img[0].cpu()))
-    WH2 = np.array([img.size(-1), img.size(-2)]) / 2
-    ax.add_artist(plt.Circle(WH2 * (label[0].cpu().detach().numpy() + 1), 2, ec='g', fill=False, lw=1.5))
-    ax.add_artist(plt.Circle(WH2 * (pred[0].cpu().detach().numpy() + 1), 2, ec='r', fill=False, lw=1.5))
-    logger.add_figure('viz', fig, global_step)
-    del ax, fig
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--log_dir')
-    parser.add_argument('-n', '--num_epoch', type=int, default=50)
-    parser.add_argument('-w', '--num_workers', type=int, default=4)
-    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
-    parser.add_argument('-c', '--continue_training', action='store_true')
-    parser.add_argument('-t', '--transform', default='Compose([ColorJitter(0.2, 0.5, 0.5, 0.2), RandomHorizontalFlip(), ToTensor()])')
 
+    parser.add_argument('--log_dir')
+    # Put custom arguments here
+    parser.add_argument('-n', '--num_epoch', type=int, default=20)
+    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
+    parser.add_argument('-g', '--gamma', type=float, default=0, help="class dependent weight for cross entropy")
+    parser.add_argument('-c', '--continue_training', action='store_true')
+    parser.add_argument('-t', '--transform',
+                        default='Compose([ColorJitter(0.4, 0.4, 0.4, 0.1), RandomHorizontalFlip(), ToTensor()])')
     args = parser.parse_args()
     train(args)
